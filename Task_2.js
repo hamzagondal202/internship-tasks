6fb2abb2431e37bd3044e936f7b922b6c835bfab
hamzagondal202 <138680064+hamzagondal202@users.noreply.github.com>
2023-07-18 16:48:01 +0500

    Add files via upload

---
+++ Task_2.js
@@ -0,0 +1,82 @@
+// // 1.	Write an arithmetic expression that calculates the area of a rectangle with a width of 5 and a height of 8.
+
+
+// let area, width = 5,height = 8 ;
+// area = width * height;
+// console.log(area);//output is 40
+
+
+
+// // 2.	Create a comparison expression that checks if two numbers are equal.
+
+
+// let a = 2, b = 5;
+// console.log(a==b);//output is false 
+
+
+
+// // 3.	Write a logical expression that checks if a number is both greater than 5 and less than 10.
+
+
+// let num = 6;
+// console.log(num > 5 && num < 10);//output is true
+
+
+
+// // 4.	Concatenate two strings using the concatenation operator.
+
+
+// let a = "Hello", b = "World";
+// console.log(a + " " + b);//output is Hello World
+
+
+
+// // 5.	Increment a variable counter by 1 using the increment operator.
+
+
+// a = 5;
+// a++;
+// console.log(a);// output is 6
+
+
+
+// // 6.	Decrement a variable counter by 1 using the decrement operator.
+
+
+// a = 7;
+// a--;
+// console.log(a);// output is 6
+
+
+
+// // 7.	Calculate the remainder when dividing 17 by 3 using the remainder operator (%).
+
+
+// let a = 17, b = 3;
+// console.log(a % b);//output is 2
+
+
+
+// // 8.	Assign the value of 10 to a variable x and use the addition assignment operator to add 5 to x.
+
+
+// x = 10;
+// console.log(x += 5);//output is 15
+
+
+
+// // 9.	Write a comparison expression using the strict equality operator that checks if a number and a string are equal.
+
+
+// a = 10;
+// b = "string";
+// console.log(a===b);// output is false
+
+
+
+// // 10.	Use the ternary operator to check if a variable isRaining is true. If true, assign the value "Take an umbrella" to a variable advice; otherwise, assign "Enjoy the day" to advice.
+
+
+// let isRaining = false;
+// let show = (isRaining ? "Take an umbrella":"Enjoy the day");
+// console.log(show);
