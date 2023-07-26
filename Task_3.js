5759a31886d741458fff7417f126cc084f392c1c
hamzagondal202 <138680064+hamzagondal202@users.noreply.github.com>
2023-07-26 20:08:49 +0500

    Add files via upload

---
+++ Task_3.js
@@ -0,0 +1,147 @@
+// 1.	Write a program that checks if a given number is positive, negative, or zero. Display the corresponding message using if-else statements.
+
+// let number = 0;
+// if (number == 0) {
+//   console.log("Number is 0");
+// } else if (number < 0) {
+//   console.log("Number is negative");
+// } else if (number > 0) {
+//   console.log("Number is positive");
+// } else {
+//   console.log("invalid input");
+// }
+
+// 2.	Create a program that determines  if a given number is a leap year or not. Display the appropriate message using if-else statements. (A leap year is divisible by 4, but not by 100, unless it is divisible by 400.)
+
+// let year = 5000;
+
+// if (year % 400 == 0) {
+//   console.log("This is a leap year");
+// } else if (year % 100 == 0) {
+//   console.log("This is not a leap year");
+// } else if (year % 4 == 0) {
+//   console.log("This is a leap year");
+// } else {
+//   console.log("This is not a leap year");
+// }
+
+// 3.	Create a program that calculates the grade based on a given score. Display the corresponding grade using if-else statements according to the following scale:
+
+// 90 or above: A
+// 80 to 89: B
+// 70 to 79: C
+// 60 to 69: D
+// Below 60: F
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// let num = 70;
+// if (num >= 90) {
+//   console.log("A Grade");
+// } else if (num >= 80) {
+//   console.log("B Grade");
+// } else if (num >= 70) {
+//   console.log("C Grade");
+// } else if (num >= 60) {
+//   console.log("D Grade");
+// } else {
+//   console.log("F Grade");
+// }
+
+// 4.	Create a program that checks the day of the week based on a given number (1 for Monday, 2 for Tuesday, etc.). Display the corresponding day using a switch statement.
+
+// Day = 7;
+
+// switch (Day) {
+//   case 1: {
+//     console.log("Monday");
+//     break;
+//   }
+//   case 2: {
+//     console.log("Tuesday");
+//     break;
+//   }
+//   case 3: {
+//     console.log("Wednesday");
+//     break;
+//   }
+//   case 4: {
+//     console.log("Thursday");
+//     break;
+//   }
+//   case 5: {
+//     console.log("Friday");
+//     break;
+//   }
+//   case 6: {
+//     console.log("Saturday");
+//     break;
+//   }
+//   case 7: {
+//     console.log("Sunday");
+//     break;
+//   }
+//   default: {
+//     console.log("invalid input");
+//   }
+// }
+
+// 5.	Write a program that checks the type of a given value and performs specific actions based on the type using a switch statement. For example, if the value is a number, multiply it by 2; if it is a string, concatenate "Hello " before it; if it is a boolean, negate it.
+
+// let value = true;
+// let check = typeof value;
+// switch (check) {
+//   case "number": {
+//     console.log(`2 * ${value} = ${2 * value}`);
+//     break;
+//   }
+//   case "string": {
+//     console.log("Hello " + value);
+//     break;
+//   }
+//   case "boolean": {
+//     console.log(!value);
+//     break;
+//   }
+
+//   default: {
+//     console.log("invalid input");
+//   }
+// }
+
+// 6.	Create a program that checks the day of the week based on a given number (1 for Monday, 2 for Tuesday, etc.). Display the corresponding day using a switch statement.
+
+// Day = 3;
+
+// switch (Day) {
+//   case 1: {
+//     console.log("Monday");
+//     break;
+//   }
+//   case 2: {
+//     console.log("Tuesday");
+//     break;
+//   }
+//   case 3: {
+//     console.log("Wednesday");
+//     break;
+//   }
+//   case 4: {
+//     console.log("Thursday");
+//     break;
+//   }
+//   case 5: {
+//     console.log("Friday");
+//     break;
+//   }
+//   case 6: {
+//     console.log("Saturday");
+//     break;
+//   }
+//   case 7: {
+//     console.log("Sunday");
+//     break;
+//   }
+//   default: {
+//     console.log("invalid input");
+//   }
+// }
